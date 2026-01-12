653b78dc1dab4a8073da8e24e80159a6b50378e0
Vitalik <v.kolesnikov.dev@gmail.com>
2026-01-12 12:00:06 +0300

    Added a form component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { TodoForm } from "./components/TodoForm";
+
+const App = () => {
+  return (
+    <>
+      <h1>Todo App</h1>
+      <TodoForm />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,8 @@
+export const TodoForm = () => {
+  return (
+    <form>
+      <input placeholder="Task To Be Done..." />
+      <button>Add</button>
+    </form>
+  );
+};
